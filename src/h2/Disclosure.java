382056814df3ac47eb5a10e7acbbd573cc047dbe
Patrick Ivarsson <Patrick.Ivarsson@gmail.com>
2012-11-20 12:42:42 +0100

    String matrices

--- src/h2/Disclosure.java
+++ src/h2/Disclosure.java
@@ -1,13 +1,18 @@
 package h2;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 public class Disclosure {
-	private String[][] disjointSets = {{"C","K","L","P","X"},
+	private String[][] disjointSets	= {{"C","K","L","P","X"},
 									   {"B","D","N","R","V"},
 									   {"G","I","M","S","W"},
-									   {"E","F","Q","T","Z"}}; 
+									   {"E","F","Q","T","Z"}};
+	private String[][] destSets		= {{"D","P","T","X","Z"},
+									   {"F","H","J","T","V"},
+									   {"G","M","V","X","Y"},
+									   {"F","I","L","M","X"},
+									   {"C","O","U","X","Z"},
+									   {"E","I","K","R","S"},
+									   {"B","J","N","R","Y"},
+									   {"C","D","Q","V","Z"}};
 	private final int b = 10; //senders
 	private final int n = 10; //receives
 	private final int N = 100; //total users of system;
