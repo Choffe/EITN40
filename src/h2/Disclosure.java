4a08ff0546b601c2650e2e826e2c9f3a65d1da28
Patrick Ivarsson <Patrick.Ivarsson@gmail.com>
2012-11-20 12:34:13 +0100

    Skeleton

--- src/h2/Disclosure.java
+++ src/h2/Disclosure.java
@@ -1,7 +1,13 @@
 package h2;
 
-public class Disclosure {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public class Disclosure {
+	private String[][] disjointSets = {{"C","K","L","P","X"},
+									   {"B","D","N","R","V"},
+									   {"G","I","M","S","W"},
+									   {"E","F","Q","T","Z"}}; 
 	private final int b = 10; //senders
 	private final int n = 10; //receives
 	private final int N = 100; //total users of system;
@@ -9,9 +15,23 @@ public class Disclosure {
 	
 	
 	public static void main(String[] args) {
-		
-		
+		learningPhase();
+		executionPhase();
+	}
+
+
+	private static void learningPhase() {
+		makeInstance();
+	}
+
+
+	private static void makeInstance() {
 		
 	}
 
+	
+	private static void executionPhase() {
+		// TODO Auto-generated method stub
+		
+	}
 }
