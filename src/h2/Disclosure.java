228d3000d36d55373fdb8991a508132b295edbaa
Patrick Ivarsson <Patrick.Ivarsson@gmail.com>
2012-11-22 12:37:17 +0100

    Code cleaned and working

--- src/h2/Disclosure.java
+++ src/h2/Disclosure.java
@@ -5,49 +5,22 @@ import java.util.HashSet;
 import java.util.Random;
 
 public class Disclosure {
-	private static String[][] disjoint = { { "C", "K", "L", "P", "X" },
-			{ "B", "D", "N", "R", "V" }, { "G", "I", "M", "S", "W" },
-			{ "E", "F", "Q", "T", "Z" } };
-
-	private static String[][] dest = { { "D", "P", "T", "X", "Z" },
-			{ "F", "H", "J", "T", "V" }, { "G", "M", "V", "X", "Y" },
-			{ "F", "I", "L", "M", "X" }, { "C", "O", "U", "X", "Z" },
-			{ "E", "I", "K", "R", "S" }, { "B", "J", "N", "R", "Y" },
-			{ "C", "D", "Q", "V", "Z" } };
-
-	private static ArrayList<HashSet<String>> disjointSets = new ArrayList<HashSet<String>>();
 	private static ArrayList<HashSet<String>> learningSets = new ArrayList<HashSet<String>>();
-	private static ArrayList<HashSet<String>> destSets = new ArrayList<HashSet<String>>();
 	private static ArrayList<HashSet<String>> databaseSets = new ArrayList<HashSet<String>>();
 	private static HashSet<String> friends = new HashSet<String>();
 	private static HashSet<String> badRecievers = new HashSet<String>();
 	private static HashSet<String> used = new HashSet<String>();
 	private static Random rnd = new Random(1337);
-	private static int counterInstance = 0;
-	private static int counterOutput = 0;
 	private static int counterNumberNeededOutput = 0;
 	
-	private static final int b = 5; // senders
-	private static final int n = 5; // receives
-	private static final int N = 26; // total users of system;
-	private static final int m = 4; // Number of friends
+	private static final int n = 5;		// Receivers
+	private static final int N = 26;	// Total users of system;
+	private static final int m = 4;		// Number of friends
 
 	public static void main(String[] args) {
-//		friends.add("C");
-//		friends.add("R");
-//		friends.add("M");
-//		friends.add("T");
-		
-//		makeSets();
-		System.out.println("hej");
 		learningPhase();
-		System.out.println("hej2");
 		executionPhase();
-		System.out.println("Real friends");
-		for (String s : friends) {
-			System.out.println(s);
-		}
-		System.out.println("Needed output sets "+ counterNumberNeededOutput);
+		printResult();
 	}
 
 	private static HashSet<String> makeRandomInstance(int offset) {
@@ -68,10 +41,8 @@ public class Disclosure {
 	}
 	
 	private static HashSet<String> makeRandomOutput() {
-		
 		HashSet<String> set = new HashSet<String>();
 		char a = (char) (65 + rnd.nextInt(m));
-		System.out.println("MAKERANDOMOUTPUT " + a);
 		set.add("" + a);
 		int j = 0;
 		while (j < n-1) {
@@ -80,26 +51,6 @@ public class Disclosure {
 			j++;
 		}
 		return set;
-
-	}
-
-	private static void makeSets() {
-		// DISJOINT
-		for (String[] array : disjoint) {
-			HashSet<String> set = new HashSet<String>();
-			for (String s : array) {
-				set.add(s);
-			}
-			disjointSets.add(set);
-		}
-		// DESTINATION
-		for (String[] array : dest) {
-			HashSet<String> set = new HashSet<String>();
-			for (String s : array) {
-				set.add(s);
-			}
-			destSets.add(set);
-		}
 	}
 
 	private static void learningPhase() {
@@ -115,37 +66,16 @@ public class Disclosure {
 		}
 	}
 
-	private static HashSet<String> makeInstance() {
-		counterInstance++;
-		return disjointSets.get(counterInstance - 1);
-	}
-
-	private static HashSet<String> makeNewOutput() {
-		if(counterOutput == 8)
-			printArray(databaseSets);
-		counterOutput++;
-		return destSets.get(counterOutput - 1);
-	}
-
 	private static void executionPhase() {
 		while (!isDone()) {
 			counterNumberNeededOutput++;
-			HashSet<String> currentSet = makeRandomOutput(); //makeNewOutput();
+			HashSet<String> currentSet = makeRandomOutput();
 			reduceSet(currentSet);
 			databaseSets.add(currentSet);
-			if (evaluate(currentSet)) {
-				for (int i = 0; i < databaseSets.size(); i++) {
-					if (evaluate(databaseSets.get(i))){
-						System.out.println("wrap " + i);
+			if (evaluate(currentSet)) 
+				for (int i = 0; i < databaseSets.size(); i++) 
+					if (evaluate(databaseSets.get(i)))
 						i = -1;
-					}
-				}
-			}
-			
-//			printArray(databaseSets);
-			printArray(learningSets);
-			System.out.println();
-			
 		}
 	}
 
@@ -159,16 +89,9 @@ public class Disclosure {
 				match = j;
 		}
 		if (dis == learningSets.size() - 1) {
-			System.out.println("/////");
-			printSet(learningSets.get(match));
-			printSet(currentSet);
-			if(match == 2 && databaseSets.indexOf(currentSet) == 1)
-				printSet(databaseSets.get(0));
 			reduceLearningSet(match, currentSet);
 			reduceDatabase();
 			databaseSets.remove(currentSet);
-			printSet(learningSets.get(match));
-			System.out.println("------");
 			return true;
 		}
 		return false;
@@ -185,10 +108,7 @@ public class Disclosure {
 	}
 
 	private static void reduceLearningSet(int match, HashSet<String> currentSet) {
-		System.out.println("a" + databaseSets.indexOf(currentSet) + " d" + match);
-		System.out.println();
 		badRecievers.addAll(difference(currentSet, learningSets.get(match)));
-
 		intersect(learningSets.get(match), currentSet);
 	}
 
@@ -199,15 +119,19 @@ public class Disclosure {
 		System.out.println();
 	}
 
-	private static void printArray(ArrayList<HashSet<String>> array) {
-		for (HashSet<String> set : array) {
-			for (String string : set) {
-				System.out.print(string);
-			}
-			System.out.println();
-		}
+	private static void printResult() {
+		HashSet<String> finalSet = new HashSet<String>();
+		for (HashSet<String> set : learningSets)
+			finalSet.addAll(set);
+		
+		System.out.println("Real friends");
+		printSet(friends);
+		System.out.println("\nCalculated friends");
+		printSet(finalSet);
+		System.out.println("\n" + (finalSet.equals(friends) ? "Attack Successful" : "Attack Failed"));
+		System.out.println("\nOutput sets needed " + counterNumberNeededOutput);
 	}
-
+	
 	private static boolean isDone() {
 		for (HashSet<String> set : learningSets) {
 			if (set.size() != 1)
@@ -222,14 +146,12 @@ public class Disclosure {
 		return a;
 	}
 
-	private static HashSet<String> intersect(HashSet<String> A,
-			HashSet<String> B) {
+	private static HashSet<String> intersect(HashSet<String> A, HashSet<String> B) {
 		A.retainAll(B);
 		return A;
 	}
 
-	private static HashSet<String> difference(HashSet<String> A,
-			HashSet<String> B) {
+	private static HashSet<String> difference(HashSet<String> A, HashSet<String> B) {
 		HashSet<String> a = new HashSet<String>(A);
 		HashSet<String> u = union(A, B);
 		a.retainAll(B);
